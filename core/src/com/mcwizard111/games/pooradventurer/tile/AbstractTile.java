77afcd66f98575941fea4c4982c95bc4ae167141
MCWizard111 <MCWizard111@live.com>
2014-07-16 20:02:20 -0400

    Adding basic code for a grid, with some crappy grass that i put together for testing purposes

---
+++ core/src/com/mcwizard111/games/pooradventurer/tile/AbstractTile.java
@@ -0,0 +1,14 @@
+package com.mcwizard111.games.pooradventurer.tile;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+public abstract class AbstractTile extends Tile {
+    public AbstractTile() {
+        super();
+    }
+
+    @Override
+    public void render(Batch batch, int x, int y) {
+
+    }
+}
